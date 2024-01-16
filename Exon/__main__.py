c15a9d12909e753da901a9d4e86db470813ba314
Yukisouma <115670605+Yukisouma@users.noreply.github.com>
2024-01-16 20:34:53 +0530

    Update __main__.py

--- Exon/__main__.py
+++ Exon/__main__.py
@@ -47,7 +47,7 @@ buttons = [
         ),
     ],
     [
-        InlineKeyboardButton(text=f"🚁 ʜᴇʟᴘ 🚁", callback_data="help_back"),
+        InlineKeyboardButton(text=f" ʜᴇʟᴘ ", callback_data="help_back"),
         InlineKeyboardButton(text=f"🥀 sᴛᴀᴛs 🥀", callback_data="stats_callback"),
     ],
     [
